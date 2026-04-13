f9c5aee61eedd940b724641a76d37a70b4441867
Danny Burke <danny.burke@cabinetoffice.gov.uk>
2026-04-13 11:46:23 +0100

    bootstrap script

---
+++ script.ps1
@@ -0,0 +1,3 @@
+winget install Git.Git --silent --accept-package-agreements --accept-source-agreements
+winget install Anthropic.ClaudeCode --silent --accept-package-agreements --accept-source-agreements
+Write-Host "Done" -ForegroundColor Green
